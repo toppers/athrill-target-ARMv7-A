7b86805bedb56aec18da0f693625039a20d4b585
tmori@esm.co.jp <tmori@esm.co.jp>
2019-12-21 23:23:27 +0900

    add todo comments

--- src/cpu/cpu_exec/op_exec_arithm.c
+++ src/cpu/cpu_exec/op_exec_arithm.c
@@ -7,13 +7,28 @@
 int arm_op_exec_add_1(struct TargetCore *core)
 {
 	arm_OpCodeFormatType_add_1 *op = &core->decoded_code->code.add_1;
+
+	if ((op->Rn == 0b1111) && (op->S == 0)) {
+		//if Rn == ‘1111’ && S == ‘0’ then SEE ADR;
+		//TODO
+		return 0;
+	}
+	else if (op->Rn == 0b1101) {
+		//if Rn == ‘1101’ then SEE ADD (SP plus immediate);
+		//TODO
+		return 0;
+	}
+	if ((op->Rn == 0b1111) && (op->S == 1)) {
+		//if Rd == ‘1111’ && S == ‘1’ then SEE SUBS PC, LR and related instructions;
+		//TODO
+		return 0;
+	}
 	uint32 imm32 = ARMExpandImm(op->imm12, CPU_ISSET_CY(cpu_get_status(core)));
 	uint32 Rn = cpu_get_reg(core, op->Rn);
 	uint32 Rd = cpu_get_reg(core, op->Rd);
 	uint32 result = -1;
 	uint32 *status = cpu_get_status(core);
 	bool passed = ConditionPassed(op->cond, *status);
-
 	if (passed != FALSE) {
 		result = Rn + imm32;
 		 if (op->Rd == CpuRegId_PC) {
@@ -22,10 +37,12 @@ int arm_op_exec_add_1(struct TargetCore *core)
 		 }
 		 else {
 			cpu_set_reg(core, op->Rd, result);
-			op_chk_and_set_carry(status, Rn, imm32);
-			op_chk_and_set_overflow(status, Rn, imm32);
-			op_chk_and_set_zero(status, result);
-			op_chk_and_set_sign(status, result);
+			if (op->S == 1) {
+				op_chk_and_set_carry(status, Rn, imm32);
+				op_chk_and_set_overflow(status, Rn, imm32);
+				op_chk_and_set_zero(status, result);
+				op_chk_and_set_sign(status, result);
+			}
 		 }
 	}
 	DBG_ADD_1(op, Rd, Rn, imm32, result, passed);

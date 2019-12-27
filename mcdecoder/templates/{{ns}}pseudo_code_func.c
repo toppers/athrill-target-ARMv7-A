a5d87d07e285ea6746a9a439ce5bcb0c9778d9d0
tmori@esm.co.jp <tmori@esm.co.jp>
2019-12-27 19:55:01 +0900

    add template2

--- mcdecoder/templates/{{ns}}pseudo_code_func.c
+++ mcdecoder/templates/{{ns}}pseudo_code_func.c
@@ -3,8 +3,13 @@
 {% for inst in machine_decoder.extras.pseudo_codes -%}
 int arm_op_exec_{{inst.name}}(struct TargetCore *core,  {{ inst.name }}_input_type *in, {{ inst.name }}_output_type *out)
 {
-	//TODO
-	return 0;
+	int ret = -1;
+	uint32 *status = cpu_get_status(core);
+	out->passed = ConditionPassed(in->cond, *status);
+	if (out->passed != FALSE) {
+		//TODO
+	}
+	out->status = *status;
 }
 {% endfor -%}
 
--- src/cpu/cpu_dec/arm_pseudo_code_func.c
+++ src/cpu/cpu_dec/arm_pseudo_code_func.c
@@ -2,6 +2,11 @@
 
 int arm_op_exec_arm_add_imm(struct TargetCore *core,  arm_add_imm_input_type *in, arm_add_imm_output_type *out)
 {
-	//TODO
-	return 0;
+	int ret = -1;
+	uint32 *status = cpu_get_status(core);
+	out->passed = ConditionPassed(in->cond, *status);
+	if (out->passed != FALSE) {
+		//TODO
+	}
+	out->status = *status;
 }
